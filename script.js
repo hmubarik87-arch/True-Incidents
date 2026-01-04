0676b40a881abc066ca4b50167d6e0a90c92239d
hmubarik87-arch <hmubarik87@gmail.com>
2026-01-04 14:59:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,15 @@ function addStory() {
     return;
   }
 
+  let shortStory = story.length > 120 ? story.substring(0, 120) + "..." : story;
+
   let card = document.createElement("div");
   card.className = "story-card";
 
   card.innerHTML = `
     <h3>${title}</h3>
-    <p>${story}</p>
+    <p class="story-text" data-full="${story}">${shortStory}</p>
+    <button class="read-btn" onclick="toggleRead(this)">Read More</button>
     <p class="category">${category} • ${anonymous ? "Anonymous" : "Shared with name"}</p>
   `;
 
@@ -22,4 +25,17 @@ function addStory() {
 
   document.getElementById("title").value = "";
   document.getElementById("story").value = "";
+}
+
+function toggleRead(btn) {
+  let p = btn.previousElementSibling;
+  let fullText = p.getAttribute("data-full");
+
+  if (btn.innerText === "Read More") {
+    p.innerText = fullText;
+    btn.innerText = "Read Less";
+  } else {
+    p.innerText = fullText.substring(0, 120) + "...";
+    btn.innerText = "Read More";
+  }
 }
